618e69bbfef66518623cad31fe693bebf7c933bf
BN2002 <doanhcms@gmail.com>
2022-05-16 00:37:52 +0700

    Configure Router/Layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
+import { Fragment } from 'react';
 function App() {
     return (
-        <div className="App">
-            <h1>React app</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        let Layout = DefaultLayout;
+                        if (route.layout === null) {
+                            Layout = Fragment;
+                        } else if (route.layout) {
+                            Layout = route.layout;
+                        }
+
+                        let Page = route.component;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
---
+++ src/pages/Home/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h2>Home PAGE</h2>;
+}
+
+export default Home;
